d4b180221dec3b0a501c409a5c5dfae4d2c45fb4
Jesse Tham <thamjesse@gmail.com>
2020-12-09 23:28:21 -0800

    Day 10: Add DP solution

--- src/main/kotlin/twenty/Day10.kt
+++ src/main/kotlin/twenty/Day10.kt
@@ -23,14 +23,30 @@ fun countWays(joltDifferences: List<Int>): Long {
     return consecutiveOnes.map { if (it == 3) 2.0.pow(it).toLong() - 1L else 2.0.pow(it).toLong() }.fold(1) { a, b -> a * b }
 }
 
+fun countWaysDp(current: Int, target: Int, adapters: Set<Int>, memo: MutableMap<Int, Long>): Long {
+    if (current == target) {
+        return 1
+    } else if (memo.contains(current)) {
+        return memo.getValue(current)
+    }
+    var product = 0L
+    for (i in 1..3) {
+        if (adapters.contains(current + i)) {
+            product += countWaysDp(current + i, target, adapters, memo)
+        }
+    }
+    memo[current] = product
+    return product
+}
 
 fun main() {
     // No repeating numbers
     // No jolt difference of two
     // Pockets of 1 are always 4 or lower
     val adapters = Resources.readFileAsList("input.txt").map { it.toInt() }.toMutableList()
+    val deviceAdapter = adapters.maxOrNull()!! + 3
     adapters.add(0)
-    adapters.add(adapters.maxOrNull()!! + 3)
+    adapters.add(deviceAdapter)
     val joltDifferences = adapters.sorted().windowed(2).map { (a, b) -> b - a }
 
     // Part 1
@@ -38,4 +54,6 @@ fun main() {
 
     // Part 2
     println(countWays(joltDifferences))
+    // Alternatively...
+    println(countWaysDp(0, deviceAdapter, adapters.toSet(), mutableMapOf()))
 }
