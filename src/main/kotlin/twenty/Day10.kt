f6fa19aff20f1dadf95920b7589feac06d2554be
Jesse Tham <thamjesse@gmail.com>
2020-12-10 21:41:04 -0800

    Day 11

--- src/main/kotlin/twenty/Day10.kt
+++ src/main/kotlin/twenty/Day10.kt
@@ -3,40 +3,55 @@ package twenty
 import util.Resources
 import kotlin.math.pow
 
-// The idea was to just count and multiply the number of subsets for groups of adapters that we could remove. Adapters that have a
-// jolt difference of 3 with either of their neighbors can't be removed no matter what.
-//
-// I have no idea if this works with anyone else's input. I noticed that my input never had a group of removable adapters
-// that was larger than 3.
-fun countWays(joltDifferences: List<Int>): Long {
-    val consecutiveOnes = mutableListOf<Int>()
-    var count = 0
-    for (num in joltDifferences) {
-        if (num == 3) {
-            if (count > 1)
-                consecutiveOnes.add(count - 1)
-            count = 0
-        } else {
-            count++
+object Day10 {
+    // The idea was to just count and multiply the number of subsets for groups of adapters that we could remove. Adapters that have a
+    // jolt difference of 3 with either of their neighbors can't be removed no matter what.
+    //
+    // I have no idea if this works with anyone else's input. I noticed that my input never had a group of removable adapters
+    // that was larger than 3.
+    fun countWays(joltDifferences: List<Int>): Long {
+        val consecutiveOnes = mutableListOf<Int>()
+        var count = 0
+        for (num in joltDifferences) {
+            if (num == 3) {
+                if (count > 1)
+                    consecutiveOnes.add(count - 1)
+                count = 0
+            } else {
+                count++
+            }
         }
+        return consecutiveOnes.map { if (it == 3) 2.0.pow(it).toLong() - 1L else 2.0.pow(it).toLong() }.fold(1) { a, b -> a * b }
     }
-    return consecutiveOnes.map { if (it == 3) 2.0.pow(it).toLong() - 1L else 2.0.pow(it).toLong() }.fold(1) { a, b -> a * b }
-}
 
-fun countWaysDp(current: Int, target: Int, adapters: Set<Int>, memo: MutableMap<Int, Long>): Long {
-    if (current == target) {
-        return 1
-    } else if (memo.contains(current)) {
-        return memo.getValue(current)
-    }
-    var product = 0L
-    for (i in 1..3) {
-        if (adapters.contains(current + i)) {
-            product += countWaysDp(current + i, target, adapters, memo)
+    fun countWaysDp(current: Int, target: Int, adapters: Set<Int>, memo: MutableMap<Int, Long>): Long {
+        if (current == target) {
+            return 1
+        } else if (memo.contains(current)) {
+            return memo.getValue(current)
         }
+        var product = 0L
+        for (i in 1..3) {
+            if (adapters.contains(current + i)) {
+                product += countWaysDp(current + i, target, adapters, memo)
+            }
+        }
+        memo[current] = product
+        return product
     }
-    memo[current] = product
-    return product
+
+    fun countWaysDpBottomUp(adapters: List<Int>): Long {
+        val dp = adapters.associateWith { 0L }.toMutableMap()
+        dp[0] = 1
+        for (adapter in adapters) {
+            for (i in 1..3) {
+                if (dp.contains(adapter + i)) {
+                    dp[adapter + i] = dp.getValue(adapter + i) + dp.getValue(adapter)
+                }
+            }
+        }
+        return dp.getValue(adapters.last())
+    }
 }
 
 fun main() {
@@ -47,13 +62,16 @@ fun main() {
     val deviceAdapter = adapters.maxOrNull()!! + 3
     adapters.add(0)
     adapters.add(deviceAdapter)
-    val joltDifferences = adapters.sorted().windowed(2).map { (a, b) -> b - a }
+    adapters.sort()
+    val joltDifferences = adapters.windowed(2).map { (a, b) -> b - a }
 
     // Part 1
     println(joltDifferences.count { it == 3 } * joltDifferences.count { it == 1 })
 
     // Part 2
-    println(countWays(joltDifferences))
+    println(Day10.countWays(joltDifferences))
     // Alternatively...
-    println(countWaysDp(0, deviceAdapter, adapters.toSet(), mutableMapOf()))
+    println(Day10.countWaysDp(0, deviceAdapter, adapters.toSet(), mutableMapOf()))
+    // Finally...
+    println(Day10.countWaysDpBottomUp(adapters))
 }
---
+++ src/main/kotlin/twenty/Day11.kt
@@ -0,0 +1,64 @@
+package twenty
+
+import util.Resources
+
+object Day11 {
+    val deltas = listOf(-1 to 0, -1 to 1, 0 to 1, 1 to 1, 1 to 0, 1 to -1, 0 to -1, -1 to -1)
+
+    fun gridEqual(prev: List<CharArray>, current: List<CharArray>): Boolean {
+        for (r in current.indices) {
+            for (c in current[r].indices) {
+                if (prev[r][c] != current[r][c])
+                    return false
+            }
+        }
+        return true
+    }
+
+    fun isOccupied(grid: List<CharArray>, r: Int, dr: Int, c: Int, dc: Int): Boolean {
+        var mr = r
+        var mc = c
+        while (mr + dr in grid.indices && mc + dc in grid[r].indices) {
+            if (grid[mr + dr][mc + dc] == '#') {
+                return true
+            } else if (grid[mr + dr][mc + dc] == 'L') {
+                break
+            }
+            mr += dr
+            mc += dc
+        }
+        return false
+    }
+
+    fun gameOfSeats(input: List<CharArray>): Int {
+        val height = input.size
+        val width = input.first().size
+        var prev = input
+        do {
+            val current = MutableList(height) { CharArray(width) { 'x' } }
+            for (r in current.indices) {
+                for (c in current[r].indices) {
+                    // To do part 1, just check adjacent seats. Also, change the numOccupied condition in the else-if case to 4
+                    val numOccupied = deltas.filter { (dr, dc) -> isOccupied(prev, r, dr, c, dc) }.size
+                    current[r][c] = if (prev[r][c] == 'L' && numOccupied == 0) {
+                        '#'
+                    } else if (prev[r][c] == '#' && numOccupied >= 5) {
+                        'L'
+                    } else {
+                        prev[r][c]
+                    }
+                }
+            }
+            val tmp = prev
+            prev = current
+        } while (!gridEqual(tmp, current))
+        return prev.map { row -> row.count { seat -> seat == '#' } }.sum()
+    }
+}
+
+fun main() {
+    val input = Resources.readFileAsList("input.txt").map { it.toCharArray() }
+
+    // Part 2
+    println(Day11.gameOfSeats(input))
+}
